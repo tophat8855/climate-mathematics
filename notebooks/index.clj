a9e4522ae5f5113b16067c54812172a0bdefd212
Heather Farley <hrfarley@gmail.com>
2025-01-16 13:43:46 -0800

    initial commit

---
+++ notebooks/index.clj
@@ -0,0 +1,18 @@
+^{:kindly/hide-code true
+  :kindly/kind :kind/hiccup}
+(ns index)
+;; # Heather's little clay notebook
+
+;; This is a playground where I'm trying to learn Clay and play with some of the scicloj stuff.
+
+;; ## Goals
+;; - Work throught the [Climate Mathematics](https://climatemathematics.sdsu.edu/) textbook,
+;; using Clay to document my progress. 
+
+;; ## About me
+;; I am a professional clojure software engineer. I am on the Board for
+;; [Clojurists Together](https://www.clojuriststogether.org/).
+
+;; - [LinkedIn](https://www.linkedin.com/in/heathermoorefarley/)
+;; - [Personal Github](https://github.com/tophat8855)
+;; - [Current Work Github](https://github.com/hmoorefarley-cni)
